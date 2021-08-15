44732bdd7ed2d796f87419c620627c152362e8d1
pgmgent-{MorgBeka} <{morgbeka@student.arteveldehs.be}>
2021-08-15 15:52:38 +0200

    soms changes

--- src/app/components/animation/SnuitertjeLeg.js
+++ src/app/components/animation/SnuitertjeLeg.js
@@ -8,7 +8,7 @@ const SnuitertjeLeg = () => {
 	return (
 		<div className="snuitertje">
 		<img alt="Been Snuitertje" className="snuitertje__leg" src={Images.Leg}></img>
-			<img alt="Snuitertje" className="snuitertj__withoutleg" src={Images.SnuitertjeWithoutLeg}></img>
+			<img alt="Snuitertje" className="snuitertje__withoutleg" src={Images.SnuitertjeWithoutLeg}></img>
 		</div>
 	)
 }
